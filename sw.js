ee3a7f1539b2be471b406137ffd14f009e28bb31
Marcos Cardoso de Oliveira <cardoso.oliveira@gmail.com>
2023-02-22 20:45:30 -0300

    Ajuste no service worker

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-import gulp from 'gulp';
-import path from 'path';
-import swPrecache from 'sw-precache';
-const rootDir = '/';
-gulp.task('generate-service-worker', callback => {
-  swPrecache.write(path.join(rootDir, 'sw.js'), {
-    staticFileGlobs: [
-      // rastreia e armazena em cache todos os arquivos que correspondem a esse padrão
-      rootDir + '/**/*.{js,html,css,png,jpg,gif}',
-    ],
-    stripPrefix: rootDir
-  }, callback);
-});
+self.addEventListener('install', e => {
+    e.waitUntil(
+      // depois que o Service Worker estiver instalado,,
+      // abra um novo cache
+      caches.open('my-pwa-cache').then(cache => {
+        // adicione todas as URLs de recursos que queremos armazenar em cache
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/script.js',
+          'skysong.css',
+        ]);
+      })
+    );
+   });
