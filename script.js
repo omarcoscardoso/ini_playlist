aa0421d9a0a396923b829f81a80765b18797f10c
Marcos Cardoso de Oliveira <cardoso.oliveira@gmail.com>
2023-02-22 21:18:32 -0300

    Ajuste no service worker

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const butInstall = document.getElementById('butInstall');
 
 /* Put code here */
 
-
-
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js');
